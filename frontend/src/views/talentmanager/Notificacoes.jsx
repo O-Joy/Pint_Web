7cd4b727eda50b6da4ef0cfae5b520939923290c
Ana Chaves <chavesrita89@gmail.com>
2026-07-06 11:30:19 +0100

    Notificações no Talent Manager

---
+++ frontend/src/views/talentmanager/Notificacoes.jsx
@@ -0,0 +1,10 @@
+import LayoutTM from './components/LayoutTM'
+import NotificacoesConteudo from '../../components/NotificacoesConteudo'
+
+export default function Notificacoes() {
+  return (
+    <LayoutTM>
+      <NotificacoesConteudo />
+    </LayoutTM>
+  )
+}
